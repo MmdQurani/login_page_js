c82d3488b8afda8457b75c0d55dc942851682891
78 mmd 69 <mmdqurani@gmail.com>
2024-03-28 16:22:25 +0330

    Change the name of the JS fileTo check the entered information, I wrote the codes and changed the name of the JS file

---
+++ js/check.js
@@ -0,0 +1,37 @@
+let usersData = [
+    {id : 1, Name : "William" , Password : "12wili12"},
+    {id : 2, Name : "Michael" , Password : "12wili12"},
+    {id : 3, Name : "Elijah" , Password : "12wili12"},
+    {id : 4, Name : "Alexander" , Password : "12wili12"},
+    {id : 5, Name : "Logan" , Password : "12wili12"}
+];
+
+console.log(usersData);
+
+let userName = document.getElementById ("user_name");
+let userPassword = document.getElementById ("user_password");
+let btnSave = document.getElementById ("btn_save");
+
+let saveUserName ;
+let saveUserPassword ;
+let checkData ;
+
+btnSave.addEventListener ("click" , () => {
+    saveUserName = userName.value;
+    saveUserPassword = userPassword.value;
+
+    checkData = usersData.some ((user) => {
+        if (user.Name === saveUserName && user.Password === saveUserPassword) {
+            userName.style.backgroundColor = "rgb(234, 255, 224)";
+            userName.style.color = "rgb(95, 161, 66)";
+            userPassword.style.backgroundColor = "rgb(234, 255, 224)";
+            userPassword.style.color = "rgb(95, 161, 66)";
+            return true;
+        }else {
+            userName.style.backgroundColor = "rgb(255, 224, 233)";
+            userName.style.color = "rgb(161, 66, 96)";
+            userPassword.style.backgroundColor = "rgb(255, 224, 233)";
+            userPassword.style.color = "rgb(161, 66, 96)";
+        }
+    });
+});
