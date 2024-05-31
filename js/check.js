ffe0f1595fdba942d8b713b3b931512f036912f1
78 mmd 69 <mmdqurani@gmail.com>
2024-05-31 21:00:10 +0330

    Refactor : Add modal box

--- js/check.js
+++ js/check.js
@@ -9,10 +9,13 @@ let usersData = [
 console.log(usersData);
 
 let $ = document;
+let containerBox = $.getElementById ("container");
 let userName = $.getElementById ("user_name");
 let userPassword = $.getElementById ("user_password");
 let btnSave = $.getElementById ("btn_save");
-let mudalBox = $.getElementById ("mudal_box");
+let modalBox = $.getElementById ("modal_box");
+let titleModal = $.getElementById ("title-modal");
+let btnClose = $.getElementById ("btn-close");
 
 let togglePassword = $.querySelector (".toggle-password");
 
@@ -25,27 +28,46 @@ btnSave.addEventListener ("click" , (event) => {
     saveUserName = userName.value.toLowerCase();
     saveUserPassword = userPassword.value.toLowerCase();
 
-    checkData = usersData.some ((user) => {
+    checkData = usersData.find ((user) => {
         if (user.Name.toLowerCase() === saveUserName && user.Password.toLowerCase() === saveUserPassword) {
-            mudalBox.classList.remove ("animationMUDAL");
-            mudalBox.classList.add ("animationMUDALend");
-            mudalBox.style.display = "none";
-            userName.style.backgroundColor = "rgb(234, 255, 224)";
-            userName.style.color = "rgb(95, 161, 66)";
-            userPassword.style.backgroundColor = "rgb(234, 255, 224)";
-            userPassword.style.color = "rgb(95, 161, 66)";
+            correctInformation ();
             return true;
         }else {
-            userName.style.backgroundColor = "rgb(255, 224, 233)";
-            userName.style.color = "rgb(161, 66, 96)";
-            userPassword.style.backgroundColor = "rgb(255, 224, 233)";
-            userPassword.style.color = "rgb(161, 66, 96)";
-            mudalBox.classList.remove ("animationMUDALend");
-            mudalBox.style.display = "flex";
-            mudalBox.classList.add ("animationMUDAL");
+            falseInformation ();
         }
     });
+    
+    if (checkData) {
+        titleModal.innerHTML = 'Correct Information';
+        btnClose.addEventListener ('click' , () => {
+            location.reload();
+        })
+    }else {
+        titleModal.innerHTML = 'False Information';
+    }
 });
+
+
+
+function correctInformation () {
+    // mudalBox.classList.remove ("animationMUDAL");
+    // mudalBox.classList.add ("animationMUDALend");
+    // mudalBox.style.display = "none";
+    userName.style.backgroundColor = "rgb(234, 255, 224)";
+    userName.style.color = "rgb(95, 161, 66)";
+    userPassword.style.backgroundColor = "rgb(234, 255, 224)";
+    userPassword.style.color = "rgb(95, 161, 66)";
+}
+
+function falseInformation () {
+    userName.style.backgroundColor = "rgb(255, 224, 233)";
+    userName.style.color = "rgb(161, 66, 96)";
+    userPassword.style.backgroundColor = "rgb(255, 224, 233)";
+    userPassword.style.color = "rgb(161, 66, 96)";
+    // mudalBox.classList.remove ("animationMUDALend");
+    // mudalBox.style.display = "flex";
+    // mudalBox.classList.add ("animationMUDAL");
+}
 
 togglePassword.addEventListener ("click" , () => {
     if (userPassword.type === "text") {
